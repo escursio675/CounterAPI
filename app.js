aa500ab4191fc83c1120740ab73d10ecc56bd48c
escursio <abhisaikia675@gmail.com>
2026-07-03 11:31:33 +0530

    added site routes to app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from 'express';
 
+import siteRoutes from './routes/sites.js';
 
 const app = express();
+
 app.use(express.json());
+
+app.use('/api/sites', siteRoutes);
 
 app.get('/', (req, res) =>{
     res.send("API running");
