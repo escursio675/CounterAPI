e664e1bdfcc1d3dea966396f09178f63bf46c823
escursio <abhisaikia675@gmail.com>
2026-07-05 10:51:36 +0530

    added counter routes

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 
 import siteRoutes from './routes/sites.js';
+import counterRoutes from './routes/counters.js';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/sites', siteRoutes);
+app.use('/api/counters', counterRoutes);
 
 app.get('/', (req, res) =>{
     res.send("API running");
